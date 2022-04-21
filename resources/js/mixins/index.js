976a54127668b5ceeff30001a2011295380735b5
Norman Huth <norman@huth.it>
2022-04-21 22:47:04 +0200

    Use `laravel-nova` assets

--- resources/js/mixins/index.js
+++
@@ -1,6 +0,0 @@
-export { mapProps } from './propTypes'
-export { default as DependentFormField } from './DependentFormField'
-export { default as FormEvents } from './FormEvents'
-export { default as FormField } from './FormField'
-export { default as HandlesValidationErrors } from './HandlesValidationErrors'
-export { Errors } from 'form-backend-validation'
